8ac878324ba81b501a7ee3c671fb874f051d2f80
UNIkeEN <2641712741@qq.com>
2024-07-18 18:51:46 +0800

    chore(frontend): update package.json and set request proxy for development

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,52 @@
 /** @type {import('next').NextConfig} */
 import { i18n } from './next-i18next.config.mjs';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
+const isDev = process.env.NODE_ENV === 'development';
 
 const nextConfig = {
   i18n,
   reactStrictMode: true,
+  async rewrites() {
+    if (isDev) {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://127.0.0.1:8000/api/:path*/',
+        },
+        {
+          source: '/auth/:path*',
+          destination: 'http://127.0.0.1:8000/auth/:path*/',
+        },
+      ];
+    }
+    return [];
+  },
+  async headers() {
+    if (isDev) {
+      return [
+        {
+          source: '/api/:path*',
+          headers: [
+            { key: 'Access-Control-Allow-Credentials', value: 'true' },
+            { key: 'Access-Control-Allow-Origin', value: '*' },
+            { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+            { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept' },
+          ],
+        },
+        {
+          source: '/auth/:path*',
+          headers: [
+            { key: 'Access-Control-Allow-Credentials', value: 'true' },
+            { key: 'Access-Control-Allow-Origin', value: '*' },
+            { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+            { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept' },
+          ],
+        },
+      ];
+    }
+    return [];
+  },
 };
 
 export default nextConfig;
