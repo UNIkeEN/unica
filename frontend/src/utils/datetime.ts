c55c432ac0e0b483faaef0cc24fc5fd598c02a6e
UNIkeEN <94227543+UNIkeEN@users.noreply.github.com>
2024-08-07 16:55:41 +0800

    feat(frontend): update util functions of datetime

--- frontend/src/utils/datetime.ts
+++ frontend/src/utils/datetime.ts
@@ -1,7 +1,45 @@
+import { useTranslation } from 'react-i18next';
+
 export const ISOtoDate = (isoString: string): string => {
-    const date = new Date(isoString);
+  const date = new Date(isoString);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+};
+
+export const formatRelativeTime = (isoString: string): string => {
+  const { t } = useTranslation();
+  const date = new Date(isoString);
+  const now = new Date();
+  const diffMilliseconds = now.getTime() - date.getTime();
+  const diffMinutes = Math.floor(diffMilliseconds / (1000 * 60));
+  const diffHours = Math.floor(diffMilliseconds / (1000 * 60 * 60));
+  const diffDays = Math.floor((now.setHours(0, 0, 0, 0) - date.setHours(0, 0, 0, 0)) / (1000 * 60 * 60 * 24));
+  
+  if (diffMinutes < 3) {
+    return t('Utils.datetime.formatRelativeTime.now');
+  } else if (diffMinutes < 60) {
+    return t('Utils.datetime.formatRelativeTime.minutes-ago', { count: diffMinutes });
+  } else if (diffHours < 24 && now.getDate() === date.getDate()) {
+    if (diffHours === 1) {
+      return t('Utils.datetime.formatRelativeTime.last-hour');
+    }
+    return t('Utils.datetime.formatRelativeTime.hours-ago', { count: diffHours });
+  } else if (diffDays === 1) {
+    return t('Utils.datetime.formatRelativeTime.yesterday');
+  } else if (now.getFullYear() === date.getFullYear()) {
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return t('Utils.datetime.formatRelativeTime.others', {
+      time: `${month}-${day}`
+    });
+  } else {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
-  };
+    return t('Utils.datetime.formatRelativeTime.others', {
+      time: `${year}-${month}-${day}`
+    });
+  }
+};
