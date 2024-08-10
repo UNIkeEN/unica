eaab305b4f77e74858b990eaf2359a195b877884
UNIkeEN <94227543+UNIkeEN@users.noreply.github.com>
2024-08-10 14:20:35 +0800

    refactor(component): add box prop to setting-option component

--- frontend/src/components/settings-option.tsx
+++ frontend/src/components/settings-option.tsx
@@ -1,7 +1,7 @@
-import { Flex, VStack, Text } from "@chakra-ui/react";
+import { Flex, VStack, HStack, Text, BoxProps } from "@chakra-ui/react";
 import React from "react";
 
-interface SettingsOptionProps {
+interface SettingsOptionProps extends BoxProps {
   title: string;
   description: string;
   titleExtra?: string;
@@ -13,17 +13,21 @@ const SettingsOption: React.FC<SettingsOptionProps> = ({
   description,
   titleExtra,
   children,
+  ...boxProps
 }) => {
   return (
-    <Flex px={6} py={2} justify="space-between" alignItems="center">
-      <VStack spacing={0.5} alignItems="start">
-        <Text fontSize="lg">
-          {title}
-        </Text>
-        <Text fontSize="sm" color="gray.500">{description}</Text>
-      </VStack>
-      {children}
-    </Flex>
+      <Flex justify="space-between" alignItems="center" {...boxProps}>
+        <VStack spacing={1} alignItems="start" overflow="hidden">
+          <HStack spacing={2} flexWrap="wrap">
+            <Text fontSize="md">
+              {title}
+            </Text>
+            {titleExtra}
+          </HStack>
+          <Text fontSize="sm" color="gray.500">{description}</Text>
+        </VStack>
+        {children}
+      </Flex>
   );
 };
 
