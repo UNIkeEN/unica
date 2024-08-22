6f1fd8a65a59c11882dd3e71689904193ac7ba8c
HsxMark <164537095+HsxMark@users.noreply.github.com>
2024-08-22 23:07:42 +0800

    feat(Login): moved the toast position and adjusted session-expired (#7)
    * feat(Login): Moved the toast position and adjusted session-expired
    
    * fix(frontend): add expired param, fix dup push
    
    ---------
    
    Co-authored-by: UNIkeEN <94227543+UNIkeEN@users.noreply.github.com>

--- frontend/src/pages/oauth/session-expired.tsx
+++ frontend/src/pages/oauth/session-expired.tsx
@@ -1,23 +1,15 @@
 import { useEffect, useContext } from 'react';
 import { useRouter } from 'next/router';
 import AuthContext from '@/contexts/auth';
-import { useToast } from '@/contexts/toast';
-import { useTranslation } from 'react-i18next';
 
 const IndexPage = () => {
   const router = useRouter();
   const authCtx = useContext(AuthContext);
-  const toast = useToast();
-  const { t } = useTranslation();
 
   useEffect(() => {
     authCtx.onLogout();
     const timer = setTimeout(() => {
-      router.push('/login');
-      toast({
-        title: t('LoginPage.toast.session-expired'),
-        status: 'warning',
-        });
+      router.push('/login?expired=true');
     }, 100);
     return () => clearTimeout(timer);
   }, [router]);
