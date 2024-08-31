17845c4f9507c731cd934a5bb900f9f98e0a0f41
UNIkeEN <sunyujie@sjtu.edu.cn>
2024-08-31 18:49:25 +0800

    refactor(css): update empty-component's style

--- frontend/src/components/empty.tsx
+++ frontend/src/components/empty.tsx
@@ -1,53 +1,55 @@
 import React from "react";
-import { Box, Text, VStack, HStack } from "@chakra-ui/react";
-import { FiInbox } from "react-icons/fi";
+import { Box, Text, VStack, HStack, BoxProps } from "@chakra-ui/react";
+import { HiOutlineInbox } from "react-icons/hi2";
 import { useTranslation } from "react-i18next";
 
-interface EmptyProps {
+interface EmptyProps extends BoxProps {
   colorScheme?: string;
   size?: "sm" | "md" | "lg" | number;
   description?: string;
   children?: React.ReactNode;
+  withIcon?: boolean;
 }
 
 const iconSizeMap: Record<"sm" | "md" | "lg", string> = {
   sm: "1.5em",
-  md: "2.5em",
-  lg: "4em",
+  md: "2em",
+  lg: "3.5em",
 };
 
 const Empty: React.FC<EmptyProps> = ({
   colorScheme = "gray",
-  size = "md",
+  size = "lg",
   description,
-  children
-
+  children,
+  withIcon = true,
+  ...boxProps
 }) => {
   const { t } = useTranslation();
   const iconSize = typeof size === "number" ? `${size}px` : iconSizeMap[size] || "2.5em";
   const textSize = typeof size === "number" ? "md" : size;
-  const isLargeSize = typeof size === "number" ? size > 40 : size === "lg";
+  const isLargeSize = typeof size === "number" ? (size > 40) : size === "lg";
 
   return (
-    <Box textAlign="center" color={`${colorScheme}.500`} p={4} borderWidth="1px" borderRadius="md">
+    <Box textAlign="center" color={`${colorScheme}.400`} p={4} {...boxProps}>
       {isLargeSize ? (
+        <VStack spacing={4} justifyContent="center" alignItems="center">
+          {withIcon && <HiOutlineInbox size={iconSize} />}
+          <Text fontSize={textSize} color={`${colorScheme}.500`}>
+            {description ? description : t("Empty.nodata")}
+          </Text>
+          {children}
+        </VStack>
+      ) : (
         <VStack spacing={4}>
-          <HStack spacing={2} justifyContent="center" alignItems="center">
-            <FiInbox size={iconSize} />
+          <HStack spacing={3} justifyContent="center" alignItems="center">
+            {withIcon && <HiOutlineInbox size={iconSize} />}
             <Text fontSize={textSize} color={`${colorScheme}.500`}>
               {description ? description : t("Empty.nodata")}
             </Text>
           </HStack>
           {children}
         </VStack>
-      ) : (
-        <HStack spacing={4} justifyContent="center" alignItems="center">
-          <FiInbox size={iconSize} />
-          <Text fontSize={textSize} color={`${colorScheme}.500`}>
-            {description ? description : t("Empty.nodata")}
-          </Text>
-          {children}
-        </HStack>
       )}
     </Box>
   );
