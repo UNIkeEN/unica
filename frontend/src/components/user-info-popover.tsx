6f3dcf134cc5304fd801323dbc3b449a1dd11fe0
HsxMark <hsxmark@outlook.com>
2024-09-06 17:24:00 +0800

    feat(popover):add two components of popover and avatar

---
+++ frontend/src/components/user-info-popover.tsx
@@ -0,0 +1,51 @@
+import {
+  Avatar,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverBody,
+  Text,
+  VStack,
+  HStack
+} from "@chakra-ui/react";
+import { UserBasicInfo } from "@/models/user";
+
+interface UserInfoPopoverProps {
+  user: UserBasicInfo;
+  trigger?: "hover" | "click";
+  avatarSize?: string;
+}
+
+const UserInfoPopover = ({
+  user,
+  trigger = "hover",
+  avatarSize = "md",
+}: UserInfoPopoverProps) => {
+
+  return (
+    <Popover placement="bottom-start" closeOnBlur trigger={trigger} isLazy>
+      <PopoverTrigger>
+        <Avatar cursor="pointer" name={user.display_name} size={avatarSize} />
+      </PopoverTrigger>
+      <PopoverContent width="auto">
+        <PopoverBody p={4}>
+          <VStack spacing={2} align="start">
+            <HStack>
+              <Text fontSize="lg" fontWeight="normal">
+                {user.display_name}
+              </Text>
+              <Text fontSize="sm" color="gray.500">
+                @{user.username}
+              </Text>
+            </HStack>
+            <Text fontSize="sm" color="gray.600">
+              {user.biography}
+            </Text>
+          </VStack>
+        </PopoverBody>
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default UserInfoPopover;
