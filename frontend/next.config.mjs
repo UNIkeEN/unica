e43e9df9a19ce84cd787331a6614053ed12b241f
UNIkeEN <2641712741@qq.com>
2024-09-21 12:51:18 +0800

    chore(frontend): support env control for frontend

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 import { i18n } from './next-i18next.config.mjs';
-import { createProxyMiddleware } from 'http-proxy-middleware';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import dotenv from 'dotenv';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 const isDev = process.env.NODE_ENV === 'development';
 
