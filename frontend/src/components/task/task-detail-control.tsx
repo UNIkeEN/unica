0a77f5b62763a51e483d1e6e2cb1ac03b2b0bd2b
baiyuansjtu <sun060319@sjtu.edu.cn>
2024-10-24 10:19:05 +0800

    feat(frontend): support delete task in task-detail-control

--- frontend/src/components/task/task-detail-control.tsx
+++ frontend/src/components/task/task-detail-control.tsx
@@ -1,3 +1,4 @@
+import React, { useContext } from 'react';
 import {
   Box,
   BoxProps,
@@ -16,28 +17,52 @@ import {
 import { Task } from '@/models/task';
 import { TaskPropertyEnums } from '@/models/task';
 import PropertyIcon from '@/components/property-icon';
-
-export const taskOperationList = [
-  {key: "duplicate", icon: LuCopy, color: "black", condition: (task: Task) => true},
-  {key: "archive", icon: LuArchive, color: "black", condition: (task: Task) => !task.archived},
-  {key: "unarchive", icon: LuArchiveRestore, color: "black", condition: (task: Task) => task.archived},
-  {key: "delete", icon: LuTrash2, color: "red", condition: (task: Task) => !task.deleted}
-]
-
-interface TaskDetailControlProps extends BoxProps {
-  task: Task;
-}
+import GenericAlertDialog from '../modals/generic-alert-dialog';
+import TaskContext from '@/contexts/task';
+import { useDisclosure } from '@chakra-ui/react';
 
 const TaskDetailControl: React.FC<TaskDetailControlProps> = ({
   task,
   ...boxProps
 }) => {
   const { t } = useTranslation();
+  const { handleDeleteTasks } = useContext(TaskContext);
+  const { isOpen: isDeleteTaskDialogOpen, onOpen, onClose } = useDisclosure();
+
+  const taskOperationList = [
+    {
+      key: "duplicate",
+      icon: LuCopy,
+      color: "black",
+      condition: (task: Task) => true,
+      onClick: () => {}
+    },
+    {
+      key: "archive",
+      icon: LuArchive,
+      color: "black",
+      condition: (task: Task) => !task.archived,
+      onClick: () => {}
+    },
+    {
+      key: "unarchive",
+      icon: LuArchiveRestore,
+      color: "black",
+      condition: (task: Task) => task.archived,
+      onClick: () => {}
+    },
+    {
+      key: "delete",
+      icon: LuTrash2,
+      color: "red",
+      condition: (task: Task) => !task.deleted,
+      onClick: () => onOpen()
+    }
+  ];
 
   return (
     <Box {...boxProps}>
       <VStack spacing={6} align="stretch">
-
         <VStack spacing={2} align="stretch">
           <Text fontSize="sm" className="secondary-text" ml={3}>
             {t("TaskDetailControl.title.local-properties")}
@@ -64,24 +89,43 @@ const TaskDetailControl: React.FC<TaskDetailControlProps> = ({
           </Text>
           <VStack spacing={0.5} align="stretch">
             {taskOperationList.map((item) => (
-              item.condition(task) && (<Button 
-                size="sm" 
-                variant="ghost" 
-                color={item.color}
-                textAlign="left" 
-                justifyContent="flex-start"
-                key={item.key}
-              >
-                <Icon as={item.icon} mr={2}/>
-                {t(`TaskDetailControl.button.${item.key}`)}
-              </Button>
-            )))}
+              item.condition(task) && (
+                <Button
+                    size="sm"
+                    variant="ghost"
+                    color={item.color}
+                    textAlign="left"
+                    justifyContent="flex-start"
+                    key={item.key}
+                    onClick={() => item.onClick(task)} 
+                >
+                    <Icon as={item.icon} mr={2} />
+                    {t(`TaskDetailControl.button.${item.key}`)}
+                </Button>
+              )
+            ))}
           </VStack>
         </VStack>
-
       </VStack>
+
+      <GenericAlertDialog
+        isOpen={isDeleteTaskDialogOpen}
+        onClose={onClose}
+        title={t('DeleteTaskDialog.dialog.title')}
+        body={t('DeleteTaskDialog.dialog.content', { taskName: task.title })}
+        btnOK={t('DeleteTaskDialog.dialog.confirm')}
+        btnCancel={t('DeleteTaskDialog.dialog.cancel')}
+        onOKCallback={async () => {
+          try {
+            await handleDeleteTasks([task.local_id]); 
+            onClose(); 
+          } catch (error) {
+            console.error("Delete task failed:", error);
+          }
+        }}
+      />
     </Box>
-  )
+  );
 }
 
 export default TaskDetailControl;
